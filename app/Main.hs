4d1729f40b8c349f1074cbd2fe66b1eb3a05a6ad
Wen Kokke <wenkokke@users.noreply.github.com>
2020-07-24 20:03:31 +0100

    Added command-line options.

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,13 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module Main where
 
 import Control.Concurrent (forkFinally)
 import Control.Concurrent.MVar
 import Control.Monad (forM, forM_)
-import Control.Search
+import qualified Control.Search as NEAT
 import Data.List (transpose)
 import Data.List.Split (chunksOf)
 import Data.Text (Text)
@@ -17,6 +18,8 @@ import Data.Maybe (catMaybes)
 import GHC.Conc (getNumProcessors)
 import qualified Language.FGG.DeBruijn as DB
 import qualified Language.FGG.Named as N
+import System.Console.GetOpt
+import System.Environment (getArgs)
 import System.Exit (ExitCode(..))
 import System.FilePath ((</>))
 import System.IO (stdout,stderr)
@@ -27,11 +30,41 @@ import System.ProgressBar
 
 -- * Main function
 
-maxDepth :: Int
-maxDepth = 15
+data Options = Options
+  { maxDepth     :: Int
+  , batchSize    :: Int
+  , numProcesses :: Maybe Int
+  , goCmd        :: String
+  , goArgs       :: [String]
+  } deriving Show
 
-batchSize :: Int
-batchSize = 100
+defaultOptions :: Options
+defaultOptions = Options
+  { maxDepth     = 15
+  , batchSize    = 100
+  , numProcesses = Nothing
+  , goCmd        = "go"
+  , goArgs       = ["run", "github.com/rhu1/fgg", "-eval=-1", "-test-monom"]
+  }
+
+options :: [OptDescr (Options -> Options)]
+options =
+  [ Option ['d'] ["max-depth"]
+    (ReqArg (\arg opts -> opts { maxDepth = read arg }) "NUM")
+    "search depth"
+  , Option ['b'] ["batch-size"]
+    (ReqArg (\arg opts -> opts { batchSize = read arg }) "NUM")
+    "number of tasks allocated to each thread"
+  , Option ['J'] ["num-processes"]
+    (ReqArg (\arg opts -> opts { numProcesses = Just . read $ arg }) "NUM")
+    "number of processors to use"
+  , Option [] ["go-cmd"]
+    (ReqArg (\arg opts -> opts { goCmd = arg }) "PATH")
+    "path to go executable"
+  , Option [] ["go-arg"]
+    (ReqArg (\arg opts -> opts { goArgs = goArgs opts ++ [arg] }) "ARG")
+    "arguments for go executable"
+  ]
 
 fgg :: [String] -> IO (ExitCode, String, String)
 fgg args = readProcessWithExitCode "go" ("run" : "github.com/rhu1/fgg" : args) ""
@@ -51,7 +84,7 @@ test_monomCommute prog = do
         [ "Source:" , progSrc , "Output:" , T.pack fggout , "Errors:" , T.pack fggerr ]
 
 wellTypedProgs :: Int -> IO [DB.Prog ()]
-wellTypedProgs depth = search' O depth (DB.checkProg' opts)
+wellTypedProgs depth = NEAT.search' NEAT.O depth (DB.checkProg' opts)
   where
     opts = DB.TCSOpts
       { DB.optMethMin     = Just 1
@@ -60,10 +93,21 @@ wellTypedProgs depth = search' O depth (DB.checkProg' opts)
       , DB.optEmptyIntMax = Just 1
       }
 
+parseOpts :: [String] -> IO (Options, [String])
+parseOpts argv =
+  case getOpt Permute options argv of
+    (o,n,[]  ) -> return (foldl (flip id) defaultOptions o, n)
+    (_,_,errs) -> ioError (userError (concat errs ++ usageInfo header options))
+  where header = "Usage: fgg-gen [OPTION...]"
+
 main :: IO ()
 main = do
-  -- Get the number of CPUs available
-  numProcessors <- getNumProcessors
+
+  -- Parse command-line arguments
+  (Options{..}, _) <- parseOpts =<< getArgs
+
+  -- Get the number of processes (or the number of processors)
+  numProcessors <- maybe getNumProcessors return numProcesses
 
   -- Divvy the well-typed programs up into batches
   batches <- chunksOf batchSize <$> wellTypedProgs maxDepth
